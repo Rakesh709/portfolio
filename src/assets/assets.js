f0006beedfcd60d945acf2c8d6c7a61951062f71
Rakesh709 <kumar.rakesh1997@outlook.com>
2025-12-19 01:37:18 +0530

    added youtube section and infographic

--- src/assets/assets.js
+++ src/assets/assets.js
@@ -1,6 +1,35 @@
 import linuxcmd from "./linuxcmd.png"
 
 
-export const assets ={
-    linuxcmd
-}
+export const assets =[
+    {
+        img: linuxcmd,
+        title : "Linux Cmd",
+        description:"A concise description that supports two lines and then truncates if too long.",
+        youtubeLink:"https://www.youtube.com/watch?v=EGc84YTMKcs",
+        tags :["react", "ui", "design", "tailwind", "video", "demo"]
+    },
+    {
+        img: linuxcmd,
+        title : "Linux Cmd",
+        description:"A concise description that supports two lines and then truncates if too long.",
+        youtubeLink:"https://www.youtube.com/watch?v=EGc84YTMKcs",
+        tags :["react", "ui", "design", "tailwind", "video", "demo"]
+    },
+    {
+        img: linuxcmd,
+        title : "Linux Cmd",
+        description:"A concise description that supports two lines and then truncates if too long.",
+        youtubeLink:"https://www.youtube.com/watch?v=EGc84YTMKcs",
+        tags :["react", "ui", "design", "tailwind", "video", "demo"]
+    },
+    {
+        img: linuxcmd,
+        title : "Linux Cmd",
+        description:"A concise description that supports two lines and then truncates if too long.",
+        youtubeLink:"https://www.youtube.com/watch?v=EGc84YTMKcs",
+        tags :["react", "ui", "design", "tailwind", "video", "demo"]
+    }
+    
+]
+    
